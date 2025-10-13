4c3e6702e2a6703d86ad9f9ca0f68fc9ed7d7fd4
Pbrown1404 <peterbrown140406@gmail.com>
2025-10-13 12:14:47 +0100

    Completed D1E1,2,3

--- src/main/java/com/corndel/nozama/DB.java
+++ src/main/java/com/corndel/nozama/DB.java
@@ -1,14 +1,17 @@
 package com.corndel.nozama;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DB {
-  /** TODO: Set the dbUrl */
-  public static final String dbUrl = null;
+  public static final Dotenv dotenv = Dotenv.load();
+  public static final String dbUrl = dotenv.get("DB_URL");
 
   public static Connection getConnection() throws SQLException {
-    /** TODO: finish this method */
-    return null;
+    var dbUrl = dotenv.get("DB_URL");
+    return DriverManager.getConnection(dbUrl);
   }
 }
--- src/main/java/com/corndel/nozama/repositories/UserRepository.java
+++ src/main/java/com/corndel/nozama/repositories/UserRepository.java
@@ -31,7 +31,27 @@ public class UserRepository {
   }
 
   public static User findById(int id) throws SQLException {
+    var query = "SELECT username, firstName, lastName, email, avatar FROM users WHERE id = " + id;
+
+    try (var con = DB.getConnection();
+         var stmt = con.createStatement();
+         var rs = stmt.executeQuery(query);) {
+
+
+      while (rs.next()) {
+        var username = rs.getString("username");
+        var firstName = rs.getString("firstName");
+        var lastName = rs.getString("lastName");
+        var email = rs.getString("email");
+        var avatar = rs.getString("avatar");
+
+        var user = new User(id, username, firstName, lastName, email, avatar);
+        return user;
+      }
+
+
+    }
     // TODO: finish this method
-    return null;
+      return null;
   }
 }
