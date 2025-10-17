f35b4894b62371009e6dcffd1ea5ef1f681b133c
Charlie-robin <charlie.robin.richardson@gmail.com>
2025-10-17 12:32:26 +0100

    Examples from 121

--- src/main/java/com/corndel/nozama/exercises/D3E1.java
+++ src/main/java/com/corndel/nozama/exercises/D3E1.java
@@ -5,57 +5,81 @@ import io.javalin.http.Context;
 
 import java.util.Map;
 
-public class D3E1 {
-  // This is our counter:
-  public static Counter counter = new Counter();
+import static io.javalin.apibuilder.ApiBuilder.*;
 
-  /**
-   * Creates a Javalin app with two endpoints.
-   *
-   * @see https://tech-docs.corndel.com/javalin/routing.html
-   * @return a configured Javalin instance
-   */
-  public static Javalin createApp() {
-    var app = Javalin.create();
-    app.get("/counter", CounterController::getCounter);
-    app.put("/counter/increment", CounterController::increment);
-    return app;
-  }
+public class D3E1 {
+    // This is our counter:
+    // SINGLETON -> ONLY ONE Counter
+    public static Counter counter = new Counter();
+
+    /**
+     * Creates a Javalin app with two endpoints.
+     *
+     * @return a configured Javalin instance
+     * @see https://tech-docs.corndel.com/javalin/routing.html
+     */
+    public static Javalin createApp() {
+        var app = Javalin.create(
+                config -> {
+                    config.router.apiBuilder(() -> {
+                        path("/counter", () -> {
+                            get("/", CounterController::getCounter);
+                            put("/increment", CounterController::increment);
+                            put("/decrement", CounterController::decrement);
+                        });
+                    });
+                });
+        // HOW CAN WE TIDY/REFACTOR OUR ENDPOINTS
+
+        return app;
+    }
+
+    public static void main(String[] args) {
+        createApp().start(8080);
+    }
 }
 
 class CounterController {
-  /**
-   * Responds with the current counter as a JSON object, e.g. { "count": 3 }.
-   */
-  public static void getCounter(Context ctx) {
-    // TODO
-    var currentCount = D3E1.counter.getCount();
-    ctx.json(Map.of("count",currentCount));
-  }
+    /**
+     * Responds with the current counter as a JSON object, e.g. { "count": 3 }.
+     */
+    public static void getCounter(Context ctx) {
+        // TODO
+        // STATIC VARIABLE -> Counter counter.getCount()
+        // WHY? -> ONE COUNTER
+        var currentCount = D3E1.counter.getCount();
+        ctx.json(Map.of("count", currentCount));
+    }
 
-  /**
-   * Increases the counter by 1 and then responds with the count.
-   */
-  public static void increment(Context ctx) {
-    // TODO
-    var newCount = D3E1.counter.getCount() + 1;
-    D3E1.counter.setCount(newCount);
-    ctx.json(Map.of("count",newCount));
-  }
+    /**
+     * Increases the counter by 1 and then responds with the count.
+     */
+    public static void increment(Context context) {
+        // TODO
+        var newCount = D3E1.counter.getCount() + 1;
+        D3E1.counter.setCount(newCount);
+        context.json(Map.of("count", newCount));
+    }
+
+    public static void decrement(Context context) {
+        int newCount = D3E1.counter.getCount() - 1;
+        D3E1.counter.setCount(newCount);
+        context.json(D3E1.counter);
+    }
 }
 
 class Counter {
-  private int count;
+    private int count;
 
-  public Counter() {
-    count = 0;
-  }
+    public Counter() {
+        count = 0;
+    }
 
-  public int getCount() {
-    return count;
-  }
+    public int getCount() {
+        return count;
+    }
 
-  public void setCount(int count) {
-    this.count = count;
-  }
+    public void setCount(int count) {
+        this.count = count;
+    }
 }
